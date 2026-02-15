f2c8e408398e643aeffa88f949f49409296d9d52
Jaroslav Hamrák <jaroslav.hamrak@globallogic.com>
2026-02-15 08:53:44 +0100

    Migrate site to PHP and localize Slovak content/styles

---
+++ includes/footer.php
@@ -0,0 +1,39 @@
+<footer id="footer">
+	<div class="inner">
+		<section>
+			<h2>Kontakt</h2>
+			<form method="post" action="#">
+				<div class="fields">
+					<div class="field half">
+						<input type="text" name="name" id="name" placeholder="Meno" />
+					</div>
+					<div class="field half">
+						<input type="email" name="email" id="email" placeholder="E-mail" />
+					</div>
+					<div class="field">
+						<textarea name="message" id="message" placeholder="Správa"></textarea>
+					</div>
+				</div>
+				<ul class="actions">
+					<li><input type="submit" value="ODOSLAŤ" class="primary" /></li>
+				</ul>
+			</form>
+		</section>
+		<section>
+			<h2>Sledujte nás</h2>
+			<ul class="icons">
+				<!-- <li><a href="#" class="icon brands style2 fa-twitter"><span class="label">Twitter</span></a></li> -->
+				<li><a href="#" class="icon brands style2 fa-facebook-f"><span class="label">Facebook</span></a></li>
+				<li><a href="#" class="icon brands style2 fa-instagram"><span class="label">Instagram</span></a></li>
+				<!-- <li><a href="#" class="icon brands style2 fa-dribbble"><span class="label">Dribbble</span></a></li> -->
+				<!-- <li><a href="#" class="icon brands style2 fa-github"><span class="label">GitHub</span></a></li> -->
+				<!-- <li><a href="#" class="icon brands style2 fa-500px"><span class="label">500px</span></a></li> -->
+				<li><a href="#" class="icon solid style2 fa-phone"><span class="label">Telefon</span></a></li>
+				<li><a href="#" class="icon solid style2 fa-envelope"><span class="label">E-mail</span></a></li>
+			</ul>
+		</section>
+		<ul class="copyright">
+			<li>&copy; Untitled. Všetky práva vyhradené</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+		</ul>
+	</div>
+</footer>
